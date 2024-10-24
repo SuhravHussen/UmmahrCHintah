bdf71136168d66c3330880ddbfbbc00f018938ed
SuhravHussen <suhravshan@gmail.com>
2024-10-24 13:27:06 +0600

    style: authors list pagination style fixed

--- client/components/authors/AuthorList.tsx
+++ client/components/authors/AuthorList.tsx
@@ -10,17 +10,21 @@ export default async function AuthorList({
   const data = await getAuthors(currentPage);
 
   return (
-    <div className="flex flex-wrap gap-2 mt-10 justify-between">
-      {data.data.length > 0 &&
-        data.data.map((author) => (
-          <AuthorCard author={author} key={author.id} />
-        ))}
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-wrap  gap-2 mt-10 justify-between">
+        {data.data.length > 0 &&
+          data.data.map((author) => (
+            <AuthorCard author={author} key={author.id} />
+          ))}
+      </div>
+
       {data.pagination.totalPage > 1 && (
         <div className="mt-8 mb-8">
           <PaginationComponent
             totalPage={data.pagination.totalPage}
             currentPage={currentPage}
             query={""}
+            sort=""
           />
         </div>
       )}
